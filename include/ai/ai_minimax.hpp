bc1071e2a754c23d943ff59375831b301bc0d0cd
Jonni Haapiainen <jonni.haapiainen@gmail.com>
2025-11-26 13:10:31 +0200

    Fix some access identifiers

--- include/ai/ai_minimax.hpp
+++ include/ai/ai_minimax.hpp
@@ -9,12 +9,11 @@ class MinimaxAI : public AIPlayer {
 public:
     MinimaxAI() = default;
 
+private:
     void _set_board(const FEN& fen) override;
     void _apply_move(const UCI& move) override;
     void _undo_move() override;
     UCI _compute_move() override;
-
-private:
     double _alpha_beta(double alpha, double beta, int depth_left);
 
 private:
--- include/ai/ai_random.hpp
+++ include/ai/ai_random.hpp
@@ -9,15 +9,15 @@ void registerRandomAI();
 
 class RandomAI : public AIPlayer {
 public:
-    RandomAI() = default;
     explicit RandomAI(const std::vector<ConfigField>& cfg);
 
+private:
+    RandomAI() = delete;
     void _set_board(const FEN& fen) override;
     void _apply_move(const UCI& move) override;
     void _undo_move() override;
     UCI _compute_move() override;
 
-private:
     Board m_board;
     std::mt19937 m_rng;
 };
